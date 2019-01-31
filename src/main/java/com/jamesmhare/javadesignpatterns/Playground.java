b6ed70ba02cd56fad8da94af7c02b6dd31f540b1
jameshare <harejamesm@gmail.com>
2019-01-30 22:36:45 -0600

    wip

--- src/main/java/com/jamesmhare/javadesignpatterns/Playground.java
+++ src/main/java/com/jamesmhare/javadesignpatterns/Playground.java
@@ -3,6 +3,7 @@ package com.jamesmhare.javadesignpatterns;
 import com.jamesmhare.javadesignpatterns.StrategyPattern.Hero;
 import com.jamesmhare.javadesignpatterns.StrategyPattern.LightningBoy;
 import com.jamesmhare.javadesignpatterns.StrategyPattern.ThunderGirl;
+import com.jamesmhare.javadesignpatterns.StrategyPattern.ThunderKick;
 
 /**
  * This class serves as an entry point for the Java Design Patterns application. It allows us to
@@ -14,11 +15,17 @@ public class Playground {
     private static Hero heroCharacterOne;
 
     public static void main(String[] args) {
+
+        // The Strategy Pattern
+
         heroCharacterOne = new LightningBoy();
         heroCharacterTwo = new ThunderGirl();
 
-        heroCharacterOne.usePower();
-        heroCharacterTwo.usePower();
+        System.out.println("Hero Character One: " + heroCharacterOne.usePower());
+        System.out.println("Hero Character Two: " + heroCharacterTwo.usePower());
+
+        heroCharacterOne.setPower(new ThunderKick());
+        System.out.println("Hero Character One: " + heroCharacterOne.usePower());
     }
 
 }
